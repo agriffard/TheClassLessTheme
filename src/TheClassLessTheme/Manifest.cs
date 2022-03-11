2723fb09ca54731911b57ed5c9f9f95ae7461d35
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-11 14:09:08 +0100

    Fixing repo link

--- src/TheClassLessTheme/Manifest.cs
+++ src/TheClassLessTheme/Manifest.cs
@@ -3,7 +3,7 @@ using OrchardCore.DisplayManagement.Manifest;
 [assembly: Theme(
     Name = "The ClassLess Theme",
     Author = "Antoine Griffard",
-    Website = "https://github.com/agriffard/TheClassLessTheme.OrchardCore",
+    Website = "https://github.com/agriffard/TheClassLessTheme",
     Version = "1.0.0",
     Description = "ClassLess theme"
 )]
